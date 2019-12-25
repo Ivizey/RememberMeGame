c42a9b32de381a9481dd7369052f0dd7ee175366
Pavel Bondar <ivizey@gmail.com>
2019-12-25 14:10:03 +0200

    Initial Commit

---
+++ RememberMeGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RememberMeGame
+//
+//  Created by Pavel Bondar on 25.12.2019.
+//  Copyright © 2019 Pavel Bondar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
